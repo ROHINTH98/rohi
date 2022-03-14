760b6495151c5923a1491eb7b3da6b63685c785c
ROHINTH98 <rohinthpkt@gmail.com>
2022-03-14 10:12:27 +0530

    rohi file

---
+++ src/org/bike/ktm.java
@@ -0,0 +1,17 @@
+package org.bike;
+
+public class ktm implements Bike {
+public void cost() {
+	System.out.println("120000");
+
+}
+public void speed() {
+
+System.out.println("120km");
+}
+public static void main(String[] args) {
+	ktm b=new ktm();
+	b.cost();
+	b.speed();
+}
+}
